acebb64b91d0bea380ede436c1c1b38804964bbc
dogmaster <cwd3469@naver.com>
2021-03-08 19:00:21 +0900

    express nunjucks

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const admin = require('./routes/admin')
+const nunjucks =  require('nunjucks')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+nunjucks.configure('template', {
+  autoescape: true,
+  express : app
 })
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World!!!!!!!!!!!!!!!!@@@@@@@@!')
+})
+
+app.use( '/admin' , admin);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
