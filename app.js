2358eda40924247f0c0a06908e3d4e4da06535ea
dogmaster <cwd3469@naver.com>
2021-03-16 11:04:28 +0900

    미들웨어

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const admin = require('./routes/admin')
 const nunjucks =  require('nunjucks')
+const logger = require('morgan')
 const app = express()
 const port = 3000
 
@@ -8,7 +9,8 @@ nunjucks.configure('template', {
   autoescape: true,
   express : app
 })
- 
+//미들웨어 셋팅
+app.use(logger('dev'));
 app.get('/', (req, res) => {
   res.send('Hello World!!!!!!!!!!!!!!!!@@@@@@@@!')
 })
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) => {
+function middleware(req,res,next){
+    console.log('첫번째 미들웨어입니다.');
+    next();
+}
+function middleware2(req,res,next){
+    console.log('두번째 미들웨어입니다.');
+    next();
+}
+router.get('/',middleware,middleware2,(req,res) => {
     res.send('admin 이후 url');
 })
 router.get('/products',(req,res) => {
