f65934a8b5f6fd0c6b9a6afa521e5f3098839dc0
dogmaster <cwd3469@naver.com>
2021-03-16 12:01:32 +0900

    from

--- routes/admin.js
+++ routes/admin.js
@@ -18,6 +18,13 @@ router.get('/products',(req,res) => {
         message: '<h1>HELLO WORLD</h1>'
     } )
 })
+router.get('/products/write',(req,res)=>{
+    res.render('admin/write.html')
+})
+router.post('/products/write',(req,res)=>{
+    res.send(req.body.price)
+    //req.body.name 제품명 ,req.body.price 가격,req.body.description 설명 req.body 다 볼수 있다.
+})
 
 
 module.exports = router;
